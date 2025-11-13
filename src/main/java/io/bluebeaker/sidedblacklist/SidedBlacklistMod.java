5c63f6f3d72f36e170d8f1af40d23df0840978c4
blue-beaker <yyy5566778899@126.com>
2025-11-13 15:33:49 +0800

    remove @Mod

--- src/main/java/io/bluebeaker/sidedblacklist/SidedBlacklistMod.java
+++ src/main/java/io/bluebeaker/sidedblacklist/SidedBlacklistMod.java
@@ -1,31 +1,20 @@
 package io.bluebeaker.sidedblacklist;
 
-import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+//@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class SidedBlacklistMod
 {
     public static final String MODID = Tags.MOD_ID;
     public static final String NAME = Tags.MOD_NAME;
     public static final String VERSION = Tags.VERSION;
 
-    public MinecraftServer server;
-
     public static Logger logger = LogManager.getLogger(MODID);
 
     public SidedBlacklistMod() {
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @EventHandler
-    public void onServerStart(FMLServerStartingEvent event){
-        this.server=event.getServer();
     }
 
     public static Logger getLogger(){
